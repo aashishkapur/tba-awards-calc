8efb806bb2dcc9898eb181da056d5a9d5b6b57bb
Aashish Kapur <me@aashishkapur.com>
2015-01-14 18:36:51 -0800

    gets events successfully

--- event-ranking.js
+++ event-ranking.js
@@ -6,7 +6,7 @@
 
 function getEvents()
 {
-	// var events = [];
+	var events = [];
 	$.ajax({
 		type:"GET",
 		url: "http://www.thebluealliance.com/api/v2/events/" + year,
@@ -17,39 +17,37 @@ function getEvents()
 
 			// console.log(data);
 			// return (data);
-			var jsonParsed = JSON.parse(data);
+			// var jsonParsed = JSON.parse(data);
+				// dont need to parse json, jquery does it automatically
 
-			for(var i = 0; i < jsonParsed.length; i++)
+			// for(var i = 0; i < jsonParsed.length; i++)
+			// {
+			// 	events.push(jsonParsed[i]['key']);
+			// }
+
+			for(var i = 0; i < data.length; i++)
 			{
-				events.push(jsonParsed[i]['key']);
+				events.push(data[i]['key']);
 			}
+
 			console.log("1");
 
 		}
 	});
 
-	// return events;
+	return events;
 }
 
 
 $(document).ready(function() {
 
 	var events = getEvents();
-	// var events = [];
-	// var data = getEvents();
-	// console.log(data);
-	// // var jsonParsed = JSON.parse(data);
-
-	// for(var i = 0; i < data.length; i++)
-	// {
-	// 	events.push(data[i]['key']);
-	// }
-
 
 	for(var i = 0; i < events.length; i++)
 	{
 		console.log(events[i]);
 	}
+
 	console.log("2");
 
 
