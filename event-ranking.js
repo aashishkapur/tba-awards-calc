83c96fed9f92830cff4b467fcbcaa4e519e5b44f
Aashish Kapur <me@aashishkapur.com>
2015-01-14 18:29:10 -0800

    added async for js

--- event-ranking.js
+++ event-ranking.js
@@ -6,39 +6,51 @@
 
 function getEvents()
 {
-	var events = [];
+	// var events = [];
 	$.ajax({
 		type:"GET",
 		url: "http://www.thebluealliance.com/api/v2/events/" + year,
 		data: {"X-TBA-App-Id": tbaID},
-		dataType: "text",
+		dataType: "json",
+		async: false,
 		success: function(data) {
 
+			// console.log(data);
+			// return (data);
 			var jsonParsed = JSON.parse(data);
 
 			for(var i = 0; i < jsonParsed.length; i++)
 			{
 				events.push(jsonParsed[i]['key']);
 			}
-			console.log("2");
+			console.log("1");
 
 		}
 	});
 
-	return events;
+	// return events;
 }
 
 
 $(document).ready(function() {
 
-
 	var events = getEvents();
+	// var events = [];
+	// var data = getEvents();
+	// console.log(data);
+	// // var jsonParsed = JSON.parse(data);
 
-				for(var i = 0; i < events.length; i++)
-			{
-				console.log(events[i]);
-			}
-console.log("1");
+	// for(var i = 0; i < data.length; i++)
+	// {
+	// 	events.push(data[i]['key']);
+	// }
+
+
+	for(var i = 0; i < events.length; i++)
+	{
+		console.log(events[i]);
+	}
+	console.log("2");
 
 
 
